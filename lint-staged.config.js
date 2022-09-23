6e19f177b1ab81b0c47f9baf8d38bc26dc0e3b3b
Tien Nguyen Dang <tien.nguyendang@outlook.com>
2022-09-23 13:52:20 +0700

    chore: add .eslintignore

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,12 +1,20 @@
 module.exports = {
-  'src/**/*.{js,jsx,ts,tsx}': (filenames) => {
-    const escapedFileNames = filenames.map((name) => name.replace(/\\/g, '\\/')).join(' ')
+  "src/**/*.{js,jsx,ts,tsx}": filenames => {
+    const escapedFileNames = filenames
+      .map(name => name.replace(/\\/g, "\\/"))
+      .join(" ");
+    console.log({ escapedFileNames });
 
-    return [`eslint --fix --quiet ${escapedFileNames}`, `git add ${escapedFileNames}`]
+    return [`eslint --fix ${escapedFileNames}`, `git add ${escapedFileNames}`];
   },
-  'src/**/*.{js,jsx,ts,tsx,scss,css}': (filenames) => {
-    const escapedFileNames = filenames.map((name) => name.replace(/\\/g, '/')).join(' ')
+  "src/**/*.{js,jsx,ts,tsx,scss,css}": filenames => {
+    const escapedFileNames = filenames
+      .map(name => name.replace(/\\/g, "/"))
+      .join(" ");
 
-    return [`prettier --write ${escapedFileNames}`, `git add ${escapedFileNames}`]
+    return [
+      `prettier --write ${escapedFileNames}`,
+      `git add ${escapedFileNames}`,
+    ];
   },
-}
+};
